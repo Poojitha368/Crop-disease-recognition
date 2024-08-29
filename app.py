9061c36b4601f54acdfe856a62403ea2560e1168
Poojitha368 <132087097+Poojitha368@users.noreply.github.com>
2024-08-29 22:47:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import streamlit as st
 import numpy as np
 import tensorflow as tf
 from PIL import Image
+import time as t
 
 model = tf.keras.models.load_model('disease_disease_recognition.h5')
 
-st.title('Crop Disease Prediction')
-st.write("Upload an image to check if the plant is affected or not.")
+st.title('🌿Crop Disease Prediction')
+st.info("Upload an image to check if the plant is affected or not.")
 
 # Upload image
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
@@ -18,11 +19,15 @@ if uploaded_file is not None:
     
     # Preprocessing the image
     img_array = np.array(image)
-    img_array = tf.image.resize(img_array, (224, 224))  # Resize image to match your model's input size
+    img_array = tf.image.resize(img_array, (224, 224)) 
     img_array = tf.cast(img_array, tf.float32) / 255.0  # Normalize the image
     img_array = tf.expand_dims(img_array, axis=0)  # Model expects a batch of images
 
-    st.write("Type")
+
+    st.subheader("Type")
+    #loading
+    with st.spinner("classifying"):
+        t.sleep(2)
     predictions = model.predict(img_array)
     
     # Assuming the model returns class probabilities, convert to class index
@@ -30,12 +35,21 @@ if uploaded_file is not None:
 
     # Display the classification based on the class index
     if class_index == 0:
-        st.write("The plant is healthy.")
+        st.subheader("The plant is healthy.")
     elif class_index == 1:
-        st.write("The plant is affected by Powdery Mildew.")
-        st.write("To treat powdery mildew on plants, farmers can:Improve Air Circulation:** Space plants adequately and prune them to reduce humidity around leaves.Water Carefully:** Water at the base and avoid wetting leaves.Use Fungicides:** Apply organic options like neem oil or sulfur-based fungicides, or chemical fungicides if needed.Remove Infected Leaves:** Regularly prune and dispose of infected foliage to prevent spread.")
+        st.subheader("The plant is affected by Powdery Mildew.")
+        st.write("To treat powdery mildew on plants, farmers can:")
+        st.write("- Improve Air Circulation: Space plants adequately and prune them to reduce humidity around leaves.")
+        st.write("- Water Carefully: Water at the base and avoid wetting leaves.")
+        st.write("- Use Fungicides: Apply organic options like neem oil or sulfur-based fungicides, or chemical fungicides if needed.")
+        st.write("- Remove Infected Leaves: Regularly prune and dispose of infected foliage to prevent spread.")
     elif class_index == 2:
-        st.write("The plant is affected by Rust.")
-        st.write("For treating rusty leaves, farmers can:Remove Infected Leaves:** Cut off and dispose of any infected leaves to prevent the fungus from spreading to healthy parts of the plant.Improve Air Circulation:** Space plants properly and prune them to reduce humidity and create airflow, which helps to prevent rust.Water at the Base:** Water plants at the soil level to keep leaves dry, as wet foliage promotes rust growth.Apply Fungicides:** Use sulfur-based or copper fungicides, or other appropriate treatments to control and prevent the spread of rust.Use Resistant Varieties:** Plant rust-resistant varieties to reduce the likelihood of rust infections.")
+        st.subheader("The plant is affected by Rust.")
+        st.write("For treating rusty leaves, farmers can:")
+        st.write("- Remove Infected Leaves: Cut off and dispose of any infected leaves to prevent the fungus from spreading to healthy parts of the plant.")
+        st.write("- Improve Air Circulation: Space plants properly and prune them to reduce humidity and create airflow, which helps to prevent rust.")
+        st.write("- Water at the Base: Water plants at the soil level to keep leaves dry, as wet foliage promotes rust growth.")
+        st.write("- Apply Fungicides: Use sulfur-based or copper fungicides, or other appropriate treatments to control and prevent the spread of rust.")
+        st.write("- Use Resistant Varieties: Plant rust-resistant varieties to reduce the likelihood of rust infections.")
     else:
         st.write("Unknown classification.")
